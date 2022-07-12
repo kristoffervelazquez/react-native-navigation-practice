cd0f5af277edf30947fa3554caa16d19e7fe4cf9
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-07-12 14:15:39 -0700

    Se agregó un drawer basico

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
+import 'react-native-gesture-handler';
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler';
 import { StackNavigator } from './src/navigator/StackNavigator';
+import MenuLateralBasico from './src/navigator/MenuLateralBasico';
 
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <MenuLateralBasico />
     </NavigationContainer>
   )
 }
---
+++ src/navigator/MenuLateralBasico.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { StackNavigator } from './StackNavigator';
+import { StyleSheet, useWindowDimensions } from 'react-native'
+import SettingsScreen from '../screens/SettingsScreen';
+
+const Drawer = createDrawerNavigator();
+
+const MenuLateralBasico = () => {
+
+    const { width } = useWindowDimensions()
+
+
+    return (
+        <Drawer.Navigator
+            screenOptions={{ drawerPosition: 'left', headerShown: false, drawerType: width >= 768 ? 'permanent' : 'front' }}
+            initialRouteName='StackNavigator'
+        >
+            <Drawer.Screen name="StackNavigator" options={{ title: 'Home' }} component={StackNavigator} />
+            <Drawer.Screen name="SettingsScreen" options={{ title: 'Settings' }} component={SettingsScreen} />
+        </Drawer.Navigator>
+    );
+}
+
+const style = {
+    color: 'green'
+}
+
+
+
+export default MenuLateralBasico
+
+const s = StyleSheet.create({})
+
+
